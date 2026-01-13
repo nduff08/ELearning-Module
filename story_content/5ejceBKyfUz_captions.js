edd425c0e67237efc20a2672431f1609b90dc7f3
nduff08 <43657385+nduff08@users.noreply.github.com>
2026-01-13 04:45:12 -0600

    Add files via upload

---
+++ story_content/5ejceBKyfUz_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.990%0D%0ABefore%20you%20go%20any%20further,%20take%20this%20short%20pop%20quiz!%0D%0A%0D%0A"}')
